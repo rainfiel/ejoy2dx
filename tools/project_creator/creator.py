2ac0297740d690fbbc48e1de90636a9324922805
rainfiel <rainfiel@gmail.com>
2015-02-04 16:59:19 +0800

    project creator(msvc only at present)

---
+++ tools/project_creator/creator.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import uuid
+import shutil
+
+helper = '''
+**************************************************
+create a new project
+creator your_prj_name
+**************************************************
+'''
+
+class foo(object):
+	def __getitem__(self, key):
+		print("key:"+key)
+		return 1
+
+
+def format_file(path, *args):
+	f = open(path, "r")
+	fmt = f.read()
+	f.close()
+	text = fmt % args
+	f = open(path, "w")
+	f.write(text)
+	f.close()
+
+
+def create_msvc(prj_name, folder):
+	builder = os.path.join(folder, "build")
+	sln = os.path.join(builder, prj_name+".sln")
+	prj_dir = os.path.join(builder, prj_name)
+	guid = uuid.uuid1()
+
+	#solution file
+	os.rename(os.path.join(builder, "example.sln"), sln)
+	format_file(sln, prj_name, prj_name, prj_name, guid)
+
+	#vc project
+	os.rename(os.path.join(builder, "example"), prj_dir)
+
+	file_list=("%s.vcxproj", "%s.vcxproj.filters", "%s.vcxproj.user")
+	for i in file_list:
+		os.rename(os.path.join(prj_dir, i%"example"), os.path.join(prj_dir, i%prj_name))
+	args=(guid, prj_name, "%(AdditionalIncludeDirectories)", "%(PreprocessorDefinitions)", "%(AdditionalDependencies)")
+	format_file(os.path.join(prj_dir, "%s.vcxproj"%prj_name), *args)
+
+def main(prj_name):
+	py_path = os.getcwd()
+	root = os.path.join(py_path, "../..")
+	tpl_dir = os.path.join(py_path, "template")
+	prj_dir = os.path.join(root, prj_name)
+	if os.path.exists(prj_dir):
+		raise Exception("the project directory is exists:"+prj_dir)
+
+	shutil.copytree(tpl_dir, prj_dir)
+
+	create_msvc(prj_name, os.path.join(prj_dir, "platform/msvc"))
+
+if __name__ == '__main__':
+	if len(sys.argv) >= 2:
+		main(sys.argv[1])
+	else:
+		print("no prj_name")
+		print(helper)
