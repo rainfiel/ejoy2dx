0a0274ff7b17c2d4d32b6c331a1c922a4c21b80c
rainfiel <rainfiel@gmail.com>
2015-02-12 16:11:23 +0800

    animation callback

--- src/ejoy2dx/animation.lua
+++ src/ejoy2dx/animation.lua
@@ -5,49 +5,55 @@ local floor = math.floor
 local AnimationManager = {}
 local AnimationFrameRate = 60
 
-
-
-local mt = {}
-mt.__index = mt
-
-function mt:update(spr)
-	if self.num_loops == 0 then
-		return false
-	end
-	self.frame = self.frame + AnimationManager.animation_frame_per_frame
-	local frame = floor(self.frame)
-	spr.frame = frame
-
-	if self.num_loops > 0 and (frame // self.frame_count) >= self.num_loops then
-		return false
-	end
-	return true
-end
-
 function AnimationManager:init(logic_frame)
 	self.animation_frame_rate = AnimationFrameRate
 	self.animation_frame_per_frame = self.animation_frame_rate / logic_frame
 	self.animations = {}
 end
 
-function AnimationManager:play(spr, config)
+function AnimationManager:play(spr, num_loops, callback)
 	assert(not self.animations[spr])
 
-	config = config or {}
-	config.start_frame = config.start_frame or 0
-	config.num_loops = config.num_loops or -1  --n for n loops, -1 for loop, 0 for gone
-	config.frame = config.start_frame
-	config.frame_count = spr.frame_count
+	spr.usr_data.anim = spr.usr_data.anim or {}
+	local config = spr.usr_data.anim
+	config.frame = 0
+	config.num_loops = num_loops or -1 --n for n loops, -1 for loop, 0 for gone
+	config.playing = true
+	config.callback = callback
+	spr.frame = 0
 
-	spr.frame = config.frame
-	local ani = setmetatable(config, mt)
-	self.animations[spr] = ani
+	self.animations[spr] = true
 end
 
 function AnimationManager:stop(spr)
-	local ani = self.animations[spr]
-	if not ani then return end
-	ani.num_loops = 0
+	if self.animations[spr] then
+		local config = spr.usr_data.anim
+		if config then
+			config.playing = false
+			config.callback = nil
+		end
+	end
+end
+
+local function update_imp(spr)
+	local config = spr.usr_data.anim
+	if not config or not config.playing then
+		return false
+	end
+
+	config.frame = config.frame + AnimationManager.animation_frame_per_frame
+	local frame = floor(config.frame)
+	spr.frame = frame
+
+	if config.num_loops > 0 and
+			(frame // spr.frame_count) >= config.num_loops then
+		if config.callback then
+			config.callback()
+			config.callback = nil
+		end
+		return false
+	end
+	return true
 end
 
 local removed = {}
@@ -55,7 +61,7 @@ local removed_cnt = 0
 function AnimationManager:update()
 	removed_cnt = 0
 	for k, v in pairs(self.animations) do
-		if not v:update(k) then
+		if not update_imp(k) then
 			removed_cnt = removed_cnt + 1
 			removed[removed_cnt] = k
 		end
