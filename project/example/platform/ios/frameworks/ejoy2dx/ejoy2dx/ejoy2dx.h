e7c3a74c7dbb8e9d9fa8df57775ab4196c188a61
rainfiel <rainfiel@gmail.com>
2015-02-13 19:23:12 +0800

    Xcode example project (unfinished)

---
+++ project/example/platform/ios/frameworks/ejoy2dx/ejoy2dx/ejoy2dx.h
@@ -0,0 +1,16 @@
+//
+//  ejoy2dx.h
+//  ejoy2dx
+//
+//  Created by rainfiel on 15-2-13.
+//  Copyright (c) 2015年 rainfiel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <lua/lua.h>
+#import <lua/lauxlib.h>
+
+@interface ejoy2dx : NSObject
+
+@end
---
+++ project/example/platform/ios/frameworks/ejoy2dx/ejoy2dx/ejoy2dx.m
@@ -0,0 +1,13 @@
+//
+//  ejoy2dx.m
+//  ejoy2dx
+//
+//  Created by rainfiel on 15-2-13.
+//  Copyright (c) 2015年 rainfiel. All rights reserved.
+//
+
+#import "ejoy2dx.h"
+
+@implementation ejoy2dx
+
+@end
