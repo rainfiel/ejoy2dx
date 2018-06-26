3a4cbc572070cb2a5f8c1439faceeddbf35fa667
rainfiel <rainfiel@gmail.com>
2018-06-26 11:45:08 +0800

    remove redefine

--- src/clib/sconn_client/lib/win_compat.h
+++ src/clib/sconn_client/lib/win_compat.h
@@ -19,37 +19,15 @@
 #endif
 #define errno WSAGetLastError()
 
+#ifndef EAGAIN
 #define EAGAIN WSATRY_AGAIN
+#endif
 #define EWOULDBLOCK WSAEWOULDBLOCK
 // In windows, connect returns WSAEWOULDBLOCK rather than WSAEINPROGRESS
+#ifndef EINPROGRESS
 #define EINPROGRESS WSAEWOULDBLOCK
-
 #endif
 
-#ifdef _WIN32
-#define snprintf c99_snprintf
-inline int c99_vsnprintf(char* str, size_t size, const char* format, va_list ap)
-{
-    int count = -1;
-
-    if (size != 0)
-        count = _vsnprintf_s(str, size, _TRUNCATE, format, ap);
-    if (count == -1)
-        count = _vscprintf(format, ap);
-
-    return count;
-}
-inline int c99_snprintf(char* str, size_t size, const char* format, ...)
-{
-    int count;
-    va_list ap;
-
-    va_start(ap, format);
-    count = c99_vsnprintf(str, size, format, ap);
-    va_end(ap);
-
-    return count;
-}
 #endif
 
 int win_getsockopt(int sockfd, int level, int optname, void *optval, socklen_t *optlen);
